01b17deedfbcbd35ac9ecf745ae63cdb2b0995e3
drobi334 <robert.drenyovszki@gmail.com>
2024-02-26 22:54:22 +0100

    Fixed NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,41 +3,53 @@ import "../styles/NavBar.css";
 
 function NavBar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [stickyClass, setStickyClass] = useState("justify-content-center");
   let ListItems = ["Home", "Technologies", "Git Repository", "Contact"];
+  // Set a width threshold for desktop/laptop monitors (e.g., 768 pixels)
+  // Set scroll treshold
+  const desktopWidthThreshold = 768;
+  const scrollThreshold = 150;
+  const isDesktopOrLaptop = window.innerWidth >= desktopWidthThreshold;
 
   useEffect(() => {
     const handleScroll = () => {
-      // Set a scroll threshold (e.g., 100 pixels)
-      const scrollThreshold = 100;
       const scrolled = window.scrollY > scrollThreshold;
       setIsScrolled(scrolled);
     };
 
-    // Attach event listener
-    window.addEventListener("scroll", handleScroll);
+    const handleResize = () => {
+      
+      handleScroll();
+      if (isDesktopOrLaptop) {
+        isScrolled ? setStickyClass("float-left") : setStickyClass("justify-content-center");
+      } else {
+        isScrolled ? setStickyClass("justify-content-center fixed-top") : setStickyClass("justify-content-center");
+      }
+    };
 
-    // Cleanup the event listener on component unmount
+    // Initial check on component mount
+    handleResize();
+    // Add event listener for scroll and resize
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+   
+    // Cleanup: remove event listeners on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [isScrolled]);
 
   return (
     <>
       <nav
-        className={`navbar navbar-expand ${
-          isScrolled ? "float-left" : "justify-content-center"
-        }`}
+        className={`navbar navbar-expand ${stickyClass}`}
       >
-        <div className="container-fluid">
-          <div
-            className={` ${
-              isScrolled ? "float-left" : "justify-content-center"
-            }`}
-          >
+        <div>
+          <div className={`${stickyClass}`}>
             <ul className="nav navbar-nav">
               {ListItems.map((item) => (
-                <li key={item} className="nav-item">
+                <li key={item} className="nav-item px-5">
                   <a className="nav-link" href="#">
                     {item}
                   </a>
