c7496b057f6fca2eeedeff5d16ad0adf7aad4943
drobi334 <robert.drenyovszki@gmail.com>
2024-08-07 18:04:44 +0200

    Fixed position of icons

--- src/components/TechStackImageBox.tsx
+++ src/components/TechStackImageBox.tsx
@@ -17,31 +17,30 @@ function TechStackImageBox() {
   ];
 
   const [isVisible, setIsVisible] = useState(false);
-  const [animationKey, setAnimationKey] = useState(0); // Key to force re-render
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (containerRef.current) {
-        const containerRect = containerRef.current.getBoundingClientRect();
-        const windowHeight = window.innerHeight;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          } else {
+            setIsVisible(false);
+          }
+        });
+      },
+      { threshold: 0.1 } // Adjust as needed
+    );
 
-        // Check if the container is in view
-        if (containerRect.top < windowHeight && containerRect.bottom > 0) {
-          setIsVisible(true);
-          setAnimationKey(prevKey => prevKey + 1); // Force re-render to restart animations
-        } else {
-          setIsVisible(false);
-        }
-      }
-    };
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
 
-    handleScroll(); // Initial check
-    window.addEventListener("scroll", handleScroll);
-
-    // Cleanup: remove event listener on component unmount
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
     };
   }, []);
 
@@ -50,7 +49,7 @@ function TechStackImageBox() {
       <div className="image-container">
         {images.map((image, index) => (
           <div
-            key={`${index}-${animationKey}`} // Use unique key to force re-render
+            key={index}
             className="image-item"
             style={{
               animationDelay: isVisible ? `${index * 0.2}s` : "0s", // Stagger appearance only when visible
